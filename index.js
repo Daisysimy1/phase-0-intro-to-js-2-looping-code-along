9c835479889ab3c02f667dd570aada77288e7cca
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-13 16:33:38 +0300

    commit tree 13b86e1ae6c28486cdece07ffda7427c4bbcc5c8

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++){
+    console.log(`I am ${age} years old. Happy birthday to me!`);
+    // debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        // debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+// Lab solutions
+
+function writeCards(arr, event) {
+    let messageArray = []
+    let thanksMessage = '';
+    for (let i = 0; i < arr.length; i++) {
+        thanksMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+        messageArray.push(thanksMessage);
+    }
+    return messageArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
